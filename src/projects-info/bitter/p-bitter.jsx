801049be6e94b1c628b0c7ba67cbbbb614000036
Anthony Cursewl <135174002+JoeCursewl@users.noreply.github.com>
2024-03-21 15:05:04 -0400

    Change portafolio

--- src/projects-info/bitter/p-bitter.jsx
+++ src/projects-info/bitter/p-bitter.jsx
@@ -1,40 +1,68 @@
 import './p-bitter.css'
-export function ProjectId({ p_name, technos, notasAll }) {
+export function ProjectId({ p_name, technos, notasAll, description, date_repo, img, link_repo, img_project, description_2, deploy }) {
     return (
         <section className="all-content-project">
             <div className="all-project">
 
-                <div className="header-project">
-                    <div className='info-antho'>
-                        <img src="/cursewl_img.jpg" alt="Bitter WEBP File" />
-                        <h1>Anthony Zerpaaa</h1>
+                <div className="flex justify-between items-start w-full flex-col md:flex-row gap-2">
+                    <div className='flex justify-center items-center gap-3'>
+                        <img className='w-[30px] rounded-[100%]' src="/cursewl_img.jpg" alt="Bitter WEBP File" />
+                        <h1>Anthony Zerpa</h1>
                     </div>
-                    <p>{p_name}</p>
-                </div>
-
-                <div className='description-project'>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non culpa nesciunt harum odit 
-                        neque quaerat obcaecati veniam suscipit consequuntur dolor error, dolorem, voluptatibus 
-                        illum, blanditiis quae. Quod maiores minima reiciendis Non culpa nesciunt harum odit 
-                        neque quaerat obcaecati veniam suscipit consequuntur dolor error, dolorem, voluptatibus 
-                        illum, blanditiis quae. Quod maiores minima reiciendis?
-                    </p>
 
                     <div>
-                        <h2>Technologies used</h2>
+                        <a href="https://github.com/JoeCursewl" target='_blank' className='u-link'>repository</a>
+                    </div>
+                </div>
 
-                        <div className='flex gap-2 justify-start items-center py-4'>
+                <div className='flex justify-center flex-col items-start gap-6 text-[#ccc]'>
+                    <p>
+                        {description}
+                    </p>
+
+                    <p>
+                        {description_2}
+                    </p>
+
+                    <div className='flex justify-start items-start w-[100%] flex-col gap-3'>
+                        <div className='flex justify-between items-center gap-4 bg-[#07070741] px-4 py-2 rounded-[12px] border border-solid border-[#4e4e4e1e] hover:border-[#e79b9b34] transition-colors'>
+                            <img className='w-[50px] rounded-[12px]' src={img} alt="Nightcraft Webp File" />
+
+                            <h2>{date_repo}</h2>
+
+                            <p>{p_name}</p>
+
+                            <div className='flex justify-center items-center'>
+                                <a href={link_repo} target='_blank'>View code</a>
+                            </div>
+                        </div>
+
+                        <div className='text-[#c96262d0] flex justify-between items-center gap-2 bg-[#07070741] px-4 py-2 rounded-[12px] border border-solid border-[#4e4e4e1e] hover:border-[#e79b9b34] transition-colors'>
+                            <p>{deploy || "Not link provied"}</p>
+                            <a href={deploy}><img className='w-4 opacity-[0.8]' target='_blank' src="/svg-projects/arrozito-img.svg" alt="SVG File" /></a>
+                        </div>
+                    </div>
+
+                    <div className='flex justify-center items-center gap-6 flex-col w-full'>
+                        <div className='flex justify-center items-center gap-2 text-[#f6f3f7]'>
+                            <img className='w-[20px]' src="/svg-projects/techno-img.svg" alt="SVG File" />
+                            <h2>Technologies used</h2>
+                        </div>
+
+                        <div className='grid grid-cols-2 gap-3 w-full sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5'>
                             {
                                 technos.map((tech) => (
-                                    <p key={tech}>{tech}</p>
+                                    <p key={tech.tecno} className='bg-[#07070777] flex justify-center items-center gap-2 px-3 p-2 rounded-[8px] border border-solid border-[#63636334] hover:border-[#e79b9b34] transition-colors w-full'>
+                                        <img className='w-5' src={tech.image} alt="Tecnology SVG File" />
+                                        {tech.tecno}
+                                        </p>
                                 ))
                             }
                         </div>
                     </div>
                 </div>
 
-            </div>  
+            </div>
         </section>
     )
 }
--- src/projects-info/brd-notes.jsx
+++ src/projects-info/brd-notes.jsx
@@ -21,6 +21,7 @@ export default function Projects() {
     const bitter_id = {
             "id": "XSdlfjew",
             "p_name": "Bitter",
+            "description_repo": ["Bitter is an app to jot down anything you want to share with friends or anyone else. Sharing poems and more with links."],
             "technologies": [
                 'TailwindCSS',
                 'HTML',
@@ -33,19 +34,22 @@ export default function Projects() {
     const nightcraft_id = {
             "id": "DdijCxqA",
             "p_name": "NightcraftMC",
+            "date_repo": "10/23",
+            "description_repo": ["Redesign of NightcraftMC's web, using new technologies and opening new spaces, just to offer a different experience."],
             "technologies": [
                 'TailwindCSS',
                 'HTML',
                 'CSS',
                 'React',
                 'React-router-dom@v6',
-                'NextUI'
                 ]
         }
 
     const clinica_id = {
             "id": "plJuwhsT",
             "p_name": "Clínica Genesis",
+            "date_repo": "10/23",
+            "description_repo": ["Genesis it's desktop application, built using Tkinter, aims to streamline clinic operations and improve patient care."],
             "technologies": [
                 'TailwindCSS',
                 'HTML',
@@ -55,19 +59,37 @@ export default function Projects() {
                 ]
         }
     
-        const lintted_id = {
+        const brd_id = {
             "id": "oKdjYhgS",
-            "p_name": "Lintted",
-            "technologies": [
-              'TailwindCSS',
-              'HTML',
-              'CSS',
-              'React',
-              'React-router-dom@v6'
+            "p_name": "Brd",
+            "date_repo": "10/23",
+            "description_repo": ["BRD is a user-friendly and feature-rich application designed to streamline restaurant operations and empower managers. It offers a centralized platform for managing various aspects of your restaurant, from inventory and staff scheduling to customer engagement and table reservations."],
+            "second_description": "BRD utilizes Object-Relational Mapping (ORM) technology. An ORM acts as a bridge between your application's programming language (like Python or Java) and the relational database (like MySQL or PostgreSQL) that stores your restaurant's data. This bridge simplifies data access and manipulation by translating between object-oriented structures in your code and the tabular structure of the database.",
+            "img_review": "/img-projects/brd-app-cutted.webp",
+            "link_repo": "https://github.com/JoeCursewl/brd-dashito",
+            "link_deploy": "https://brd.breadriuss.store/login",
+            "technologies": [{
+                "tecno": "HTML",
+                "image": "/technologies/html-img.svg"
+            },
+            {
+                "tecno": "CSS",
+                "image": "/technologies/css-img.svg"
+            },
+            {
+                "tecno": "NestJS",
+                "image": "/technologies/nest-img.svg"
+            },
+            {
+                "tecno": "React",
+                "image": "/technologies/react-img.svg"
+            },
+            {
+                "tecno": "Prisma",
+                "image": "/technologies/prisma-img.svg"
+            }
             ]
           };
-          
-          console.log(lintted_id.technologies);
 
 
     return (
@@ -75,7 +97,7 @@ export default function Projects() {
                 {param === bitter_id.id ? <ProjectId p_name={bitter_id.p_name} technos={bitter_id.technologies} /> : ""}
                 {param === nightcraft_id.id ? <ProjectId p_name={nightcraft_id.p_name} technos={nightcraft_id.technologies}/> : ""}
                 {param === clinica_id.id ? <ProjectId p_name={clinica_id.p_name} technos={clinica_id.technologies} /> : ""}
-                {param === lintted_id.id ? <ProjectId p_name={lintted_id.p_name} technos={lintted_id.technologies}/> : ""}
+                {param === brd_id.id ? <ProjectId p_name={brd_id.p_name} technos={brd_id.technologies} description={brd_id.description_repo} date_repo={brd_id.date_repo} img={brd_id.img_review} link_repo={brd_id.link_repo} description_2={brd_id.second_description} deploy={brd_id.link_deploy}/> : ""}
         </motion.section>
     )
 }
